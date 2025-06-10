6aa5cf0c28b8758c1c5c2e8484145a94d769bd74
Homura <junyujin@umich.edu>
2025-06-10 15:17:33 -0400

    Update

--- Monitor.ino
+++ Monitor.ino
@@ -5,9 +5,16 @@
 #define TCA9548A_ADDR 0x70 // Address of multiplexer
 #define AD7746_ADDR 0x48 // Address of sensor
 
+// for result output
+struct Result{
+    float mean;
+    float std_dev;
+    int count;
+};
+
 AD7746 capSensor;
 
-const int numReadings = 11; // Number of samples for mean filtering
+const int numReadings = 32; // Number of samples for mean filtering
 
 void tca_select(uint8_t bus) {
     if (bus > 7) return;
@@ -49,39 +56,59 @@ void sensor_init() {
 
 }
 
-// **Function to get filtered capacitance value using median filtering**
-float readFilteredCapacitance() {
-    uint32_t readings[numReadings];
 
+
+// **Function to get filtered capacitance value using median filtering**
+Result readFilteredCapacitance() {
+    Result r;
+    float readings[numReadings];
+    float fReadings[numReadings];
+    uint32_t temp = 0;
     // Take multiple readings for noise reduction
     for (int i = 0; i < numReadings; i++) {
-        readings[i] = capSensor.getCapacitance();
-        delay(10); // Small delay between readings
+        temp = capSensor.getCapacitance();
+        // Convert raw data to capacitance in pF (adjusting for ±4 pF range, starting at 8 pF)
+        readings[i] = (float) temp * (4.0 / 0x800000) + 4.0;
+        delay(1);// Small delay between readings 
     }
 
     // Sort the readings to get the median
-    for (int i = 0; i < numReadings - 1; i++) {
-        for (int j = i + 1; j < numReadings; j++) {
-            if (readings[i] > readings[j]) {
-                uint32_t temp = readings[i];
-                readings[i] = readings[j];
-                readings[j] = temp;
-            }
-        }
-    }
 
-    uint32_t medianReading = readings[numReadings / 2]; // Get median value
-    uint32_t meanReading;
+    float mean;
     int hi = numReadings - 1;
     int lo = 0;
-    int sum = 0;
-    for(int i = lo + 2; i < hi - 1; i++) {
+    float sum = 0;
+    for(int i = 0; i < numReadings - 1; i++) {
         sum += readings[i];             
     }
-    meanReading = sum / (hi - lo - 3);
-    // Convert raw data to capacitance in pF (adjusting for ±4 pF range)
-    float capValue = ((float)meanReading - 0x800000) * (4.0 / 0x800000);
-    return capValue + 8.0; // Adding 8pF center offset
+    mean = sum / numReadings;
+
+    float variance = 0;
+    for (int i = 1; i < numReadings - 1; i++) {       
+        variance += pow(readings[i] - mean, 2);
+    }
+    variance /= numReadings;
+    float std_dev = sqrt(variance);
+    float lower_bound = mean - 3 * std_dev;
+    float upper_bound = mean + 3 * std_dev;
+    
+    int count = 0;
+    for (int i = 0; i < numReadings - 1; i++) {
+        if (readings[i] >= lower_bound && readings[i] <= upper_bound) {
+            fReadings[count] = readings[i];
+            count += 1;
+        }
+    }
+    float fSum = 0;
+    for(int i = 0; i < count - 1; i++) {
+        fSum += fReadings[i];             
+    }
+    float fMean = 0;
+    fMean = fSum / count;
+    r.mean = fMean;
+    r.std_dev = std_dev;
+    r.count = count;
+    return r;
 }
 
 
@@ -100,29 +127,36 @@ void setup() {
 void loop() {
     tca_select(0);
     capSensor.writeCapSetupRegister(AD7746_CAPEN);
-    float capacitance0 = readFilteredCapacitance();
+    delay(25);
+    Result capacitance0 = readFilteredCapacitance();
+    // delay(250);
     capSensor.writeCapSetupRegister(AD7746_CAPEN | AD7746_CIN2);
-    float capacitance1 = readFilteredCapacitance();
+    delay(25);
+    Result capacitance1 = readFilteredCapacitance();
     capSensor.writeCapSetupRegister(0);
-    
+// The problem is when press 0, 0 and 1 both increase, when press 1 nothing happens.
+// Delay must be kept in capsetup & read at around 20
+
     tca_select(1);
     capSensor.writeCapSetupRegister(AD7746_CAPEN);
-    float capacitance2 = readFilteredCapacitance();
+    delay(25);
+    Result capacitance2 = readFilteredCapacitance();
     capSensor.writeCapSetupRegister(AD7746_CAPEN | AD7746_CIN2);
-    float capacitance3 = readFilteredCapacitance();
+    delay(25);
+    Result capacitance3 = readFilteredCapacitance();
     capSensor.writeCapSetupRegister(0);
 
     Serial.print("Capacitance0: ");
-    Serial.print(capacitance0);
+    Serial.print(capacitance0.mean);
     Serial.println(" pF");
     Serial.print("Capacitance1: ");
-    Serial.print(capacitance1);
-    Serial.println(" pF");
+    Serial.print(capacitance1.mean);
+    Serial.print(" pF");
     Serial.print("Capacitance2: ");
-    Serial.print(capacitance2);
+    Serial.print(capacitance2.mean);
     Serial.println(" pF");
     Serial.print("Capacitance3: ");
-    Serial.print(capacitance3);
+    Serial.print(capacitance3.mean);
     Serial.println(" pF");
     Serial.println(" ");
 
